a3b45c2b6c9dfbbce997e280e4e89896f1554d1b
Davi Falcao <davifaf3106@gmail.com>
2025-09-27 10:41:06 -0300

    Coloquei todos os css juntos

--- HomePage/homepage.js
+++ HomePage/homepage.js
@@ -5,6 +5,21 @@ let botaoExponenciacao = [...document.querySelectorAll('#BotaoExponenciacao')];
 
 botaoMultiplicacao.forEach((n) => {
     n.addEventListener('click', () => {
-        window.location.href = '../Relogio/Relogio.html';
+        window.location.href = '../HomePage/Multiplicacao/multiplicacao.html';
+    });
+});
+
+
+
+botaoSoma.forEach((n) => {
+    n.addEventListener('click', () => {
+        window.location.href = '../HomePage/Soma/soma.html';
+    });
+});
+
+
+botaoExponenciacao.forEach((n) => {
+    n.addEventListener('click', () => {
+        window.location.href = '../HomePage/Exponenciacao/exponenciacao.html';
     });
 });
